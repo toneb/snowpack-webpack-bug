124a7c0ac35ef5aee2f7d9c19a0e11839188a1ab
toneb <tone.bone@protonmail.com>
2020-08-29 13:53:39 +0200

    Minimal webpack bug repro sample.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+import styles from "./test.module.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className={styles.demo}>
+      Classname: {styles.demo ?? "undefined"}
+    </div>
   </React.StrictMode>,
   document.getElementById('root'),
 );
